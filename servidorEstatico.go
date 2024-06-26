a15fc381ca969b8ee4c534fac368245ea9c64ff4
Gabriel <lopes.biel2588@gmail.com>
2024-06-26 00:05:23 -0300

    first commit

--- servidorEstatico.go
+++ servidorEstatico.go
@@ -5,36 +5,21 @@ import (
 	"net/http"
 )
 
-
-func ola(w http.ResponseWriter, req *http.Request){
+func ola(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Ola mundo")
 }
 
-func cabecalho(w http.ResponseWriter, req *http.Request){
-	for nome, cabecalho := reange cabecalho{
-		for _, c := range cabecalho{
+func cabecalho(w http.ResponseWriter, req *http.Request) {
+	for nome, cabecalho := range req.Header {
+		for _, c := range cabecalho {
 			fmt.Fprintf(w, "%v: %v\n", nome, c)
 		}
 	}
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/ola", ola)
-    http.HandleFunc("/cabecalho", cabecalho)
-
-	
-    http.ListenAndServe("localhost:8080", nil)
-    fmt.Println("Servidor rodando na porta 8080")
-    fmt.Scanln()
-    fmt.Println("Servidor parado")
-    fmt.Scanln()
-    fmt.Println("Servidor rodando na porta 8080")
-    fmt.Scanln()
-    fmt.Println("Servidor parado")
-    fmt.Scanln()
-    fmt.Println("Servidor rodando na porta 8080")
-    fmt.Scanln()
-    fmt.Println("Servidor parado")
-    fmt.Scanln()
+	http.HandleFunc("/cabecalho", cabecalho)
+	http.ListenAndServe("localhost:8080", nil)
     fmt.Println("Servidor rodando na porta 8080")
 }
